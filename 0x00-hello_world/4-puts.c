946c9330778027fa948e6851687af2d5837f1167
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2023-08-10 15:51:14 -0700

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main function 
+ *
+ * print a sentence 
+ *
+ * retun 0 
+*/
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");	
+        return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main function
+ *
+ * size of each one
+ *
+ * return 0
+*/
+
+int main()
+{
+    printf("size of a char: %lu byte(s)\n" ,sizeof(char));
+    printf("size of an int: %lu byte(s)\n" ,sizeof(int));
+    printf("size of a long int: %lu byte(s)\n" ,sizeof(long int));
+    printf("size of a long long int: %lu byte(s)\n" ,sizeof(long long int));
+    printf("size of a float: %lu byte(s)\n" ,sizeof(float));
+    return (0);
+}
