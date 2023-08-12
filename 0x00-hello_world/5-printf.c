2fe08b91a3b4cfcd8b5b4b8e420f2eec0068349d
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2023-08-12 03:01:25 -0700

    first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main functon 
+ * main functon
  * print a sentence
  * return 0
 */
 
 int main(void)
 {
-   printf("with proper grammar, but the outcome is a piece of art,\n");
-   return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
